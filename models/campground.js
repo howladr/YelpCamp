e1667fc4a0cd3dc03c1f6caa0eb80a98f3344ac8
Raiyan <raiyanhowlader@gmail.com>
2020-10-02 21:25:41 +0000

    add YelpCamp final version files

---
+++ models/campground.js
@@ -0,0 +1,33 @@
+var mongoose = require("mongoose");
+ 
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+	price: String,
+   image: String,
+   description: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		}, 
+		username: String
+	}, 
+	comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+
+//Campground pre-hook
+const Comment = require("./comment");
+campgroundSchema.pre("remove", async function(){
+	await Comment.deleteMany({
+		_id: {
+			$in: this.comments
+		}
+	});
+});
+ 
+module.exports = mongoose.model("Campground", campgroundSchema);
